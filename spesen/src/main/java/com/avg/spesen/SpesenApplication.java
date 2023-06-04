0c97d3f228c9ce8280834487c06ea86d6a068bcf
buniql <bunertlukas@gmail.com>
2023-06-04 21:28:11 +0200

    initial commit

---
+++ spesen/src/main/java/com/avg/spesen/SpesenApplication.java
@@ -0,0 +1,13 @@
+package com.avg.spesen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpesenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpesenApplication.class, args);
+	}
+
+}
---
+++ spesen/src/test/java/com/avg/spesen/SpesenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avg.spesen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpesenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
